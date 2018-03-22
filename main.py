bf4e44cadc1e56100dfb2d09642d7bccb1989f88
Alex Thompson <awt1088@gmail.com>
2018-03-21 19:23:00 -0700

    updated documenatation

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+#!usr/bin/env/python
+
+# File:           main.py
+# Author:         Alex Thompson
+# Github:         palex88
+# Date Created:   2018-03-19
+# Date Modified:  2018-03-21
+# Python Version: 3.6
+#
+# Purpose:        This is a program that visualizes pcap data obtained from Wireshark. It plots the count of the types
+#                 of packets and plots them in a bar graph.
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def run():
+    """
+    Main function run that visualizes the data.
+    
+    :return:
+    """
     pcap_data = pd.read_csv('packet_metadata.csv', index_col='No.')
 
     df = pcap_data.groupby('Protocol')['Protocol'].count()
