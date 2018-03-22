8d90ec02452664358f811d65e1811ad8bd86c869
Alex Thompson <awt1088@gmail.com>
2018-03-21 19:05:53 -0700

    update to main to show the data from packet_metadata csv

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from vpython import *
+import pandas as pd
+import matplotlib.pyplot as plt
 
-box()
+
+def run():
+    pcap_data = pd.read_csv('packet_metadata.csv', index_col='No.')
+
+    df = pcap_data.groupby('Protocol')['Protocol'].count()
+
+    df.plot(kind='bar')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    run()
